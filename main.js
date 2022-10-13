13654a5b16b0e5b4fcac4c882c78b764985d8835
GRE3NDE <65571952+GRE3NDE@users.noreply.github.com>
2022-10-13 15:31:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var kek = document.getElementById('no');
+
+kek.addEventListener('click', () => {
+  document.getElementById('res').classList.add('lol');
+  document.getElementById('res').classList.remove('res');
+});
